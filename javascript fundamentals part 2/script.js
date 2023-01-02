d34abdaeaee09ac30daded84cf22698bbae3a5c5
AhmadAsif9839 <ahmadasif9839@gmail.com>
2023-01-02 14:11:17 +0500

    second commit

---
+++ javascript fundamentals part 2/script.js
@@ -0,0 +1,71 @@
+/////////////// Using strict mode /////////////////
+"use strict";
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("You are able to drive");
+
+// const interface = false; error (reserved word)
+// const private = false; error (reserved word)
+
+////////////////// functions /////////////////////
+
+function greeter(myName) {
+  console.log(`Hello ${myName}`);
+}
+
+// function calling, invoking
+greeter("Ahmad");
+
+function printDetail(myName, myAge, myJob) {
+  console.log(`My name is ${myName}`);
+  console.log(`My Age: ${myAge}`);
+  console.log(`My job ${myJob}`);
+}
+
+printDetail("Ahmad", 20, "programmer");
+
+function juiceProducer(apples, oranges) {
+  console.log(apples, oranges);
+
+  const juice = `You want ${apples} apples juices
+    and ${oranges} orange juices`;
+
+  return juice;
+}
+
+juiceProducer(2, 0);
+const appleJuice = juiceProducer(2, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = juiceProducer(2, 4);
+console.log(appleOrangeJuice);
+
+// function declaration //
+// const age1 = calcAge1(2002);
+function calcAge1(birthYear) {
+  return 2022 - birthYear;
+}
+const age1 = calcAge1(2002);
+
+// function expression
+const calcAge2 = function (birthYear) {
+  return 2022 - birthYear;
+};
+
+const age2 = calcAge2(2002);
+console.log(age1, age2);
+
+// Arrow functions
+const calcAge3 = (birthYear) => 2022 - birthYear;
+console.log(calcAge3(2002));
+
+const yeatsUntilRetirement = (birthYear, firstName) => {
+  const age = calcAge3(2002);
+  const retirementAge = 60 - age;
+  return `${firstName} retires in ${retirementAge} years`;
+};
+
+const calcYearsLeft = yeatsUntilRetirement(2002, "Ahmad");
+console.log(calcYearsLeft);
