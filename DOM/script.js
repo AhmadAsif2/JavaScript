f29a4c6c67166b93aad59adb13451c129c185f4b
AhmadAsif9839 <ahmadasif9839@gmail.com>
2023-01-07 21:16:35 +0500

    refactoring game code

--- DOM/script.js
+++ DOM/script.js
@@ -4,10 +4,10 @@
 // console.log(document.querySelector('.message').textContent);
 
 /*
-    The DOM is not part of the JavaScript language, but is instead a Web API used to build websites. (Document object Model)
+    The DOM (Document object Model) is not part of the JavaScript language, but is instead a Web API used to build websites. 
 
     Document is the starting point of any web page through we
-    can access root elements of html) to manipulate html elements (A tree like herarchy)
+    can access elements of html) to manipulate html elements (A tree like herarchy)
 
 
 */
@@ -26,16 +26,19 @@ let score = 20;
 let highScore = 0;
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number';
+    displayMessage('No number');
   }
   //   When player wins
   else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number You won!!';
-
+    displayMessage('Correct Number You won!!');
     if (score > highScore) {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
@@ -44,23 +47,12 @@ document.querySelector('.check').addEventListener('click', () => {
     // Manipulating CSS
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('body').style.backgroundColor = '#60b347';
-  }
-  //   When guess is high
-  else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score == 0) {
-      document.querySelector('.message').textContent = 'You lose!';
+      displayMessage('You lose!');
     } else if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high number';
-      score--;
-      document.querySelector('.score').textContent = score;
-    }
-
-    // When guess is low
-  } else if (guess < secretNumber) {
-    if (score == 0) {
-      document.querySelector('.message').textContent = 'You lose';
-    } else if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low number';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high number' : 'Too low number';
       score--;
       document.querySelector('.score').textContent = score;
     }
@@ -71,7 +63,7 @@ document.querySelector('.check').addEventListener('click', () => {
 document.querySelector('.again').addEventListener('click', () => {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
